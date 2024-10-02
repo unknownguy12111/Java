cf2f35d5b521fe2104b3fbda97b19b290987222f
Tripti Rai <159754601+Triptirai18@users.noreply.github.com>
2024-10-02 16:15:02 +0530

    Add files via upload
    Reversing the string

---
+++ reverseString.java
@@ -0,0 +1,15 @@
+public class reverseString {
+        public static void main(String[] args) {
+            String original = "Hello , Tripti";
+            char[] charArray = original.toCharArray();
+            int left = 0;
+            int right = charArray.length -1;
+            while(left < right) {
+                char temp = charArray[left];
+                charArray[left] = charArray[right];
+                charArray[right] = temp;
+            }
+            String reversed = new String(charArray);
+            System.out.println("Reversed Array"+reversed);
+        }
+}

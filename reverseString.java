a63c9a208bb76c09ee6fe107cf8294e4c15d133e
Tripti Rai <159754601+Triptirai18@users.noreply.github.com>
2024-10-02 16:23:23 +0530

    Update reverseString.java

--- reverseString.java
+++ reverseString.java
@@ -1,6 +1,6 @@
 public class reverseString {
         public static void main(String[] args) {
-            String original = "Hello , Tripti";
+            String original = "Hello , Tripti !!";
             char[] charArray = original.toCharArray();
             int left = 0;
             int right = charArray.length -1;
@@ -8,8 +8,10 @@ public class reverseString {
                 char temp = charArray[left];
                 charArray[left] = charArray[right];
                 charArray[right] = temp;
+                left++;
+                right--;
             }
             String reversed = new String(charArray);
-            System.out.println("Reversed Array"+reversed);
+            System.out.println("Reversed Array: "+reversed);
         }
 }
